5d5481bb8a8742cc4de9e2c16f8e9c8a4ebd8485
Alex Kiryxa <kiryxa@Alexs-iMac.local>
2016-12-19 11:43:43 +0200

    Greet new user and alert others

--- server/server.js
+++ server/server.js
@@ -14,15 +14,33 @@ app.use(express.static(publicPath));
 io.on('connection', (socket) => {
     console.log('New user connected');
 
+    socket.emit('newMessage', {
+        from: 'Admin',
+        text: 'Welcome to chat app',
+        createdAt: new Date().getTime()
+    });
+
+    socket.broadcast.emit('newMessage', {
+        from: 'Admin',
+        text: 'New user joined us',
+        createdAt: new Date().getTime()
+    });
+
     socket.on('createMessage', (message) => {
         console.log('createMessage', message);
-
         //io.emit emits the event to every connection, socket.emit emits the event to single connection
         io.emit('newMessage', {
             from: message.from,
             text: message.text,
             createdAt: new Date().getTime()
         });
+
+        //socket.broadcast.emit send the event to everybody but this socket
+        // socket.broadcast.emit('newMessage', {
+        //     from: message.from,
+        //     text: message.text,
+        //     createdAt: new Date().getTime()
+        // });
     });
 
     socket.on('disconnect', (socket) => {
