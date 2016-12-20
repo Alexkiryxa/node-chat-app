00f731d3757c5cf2bd3c95ca806aef8952d8e9b0
Alex Kiryxa <kiryxa@Alexs-iMac.local>
2016-12-20 14:50:26 +0200

    Send messages to only people in same room

--- server/server.js
+++ server/server.js
@@ -38,9 +38,13 @@ io.on('connection', (socket) => {
     });
 
     socket.on('createMessage', (message, callback) => {
-        console.log('createMessage', message);
+        let user = users.getUser(socket.id);
+
+        if ( user && isRealString(message.text) ) {
+            io.to(user.room).emit('newMessage', generateMessage(user.name, message.text));
+        }
         //io.emit emits the event to every connection, socket.emit emits the event to single connection
-        io.emit('newMessage', generateMessage(message.from, message.text));
+
         callback();
         //socket.broadcast.emit send the event to everybody but this socket
         // socket.broadcast.emit('newMessage', {
@@ -51,7 +55,11 @@ io.on('connection', (socket) => {
     });
 
     socket.on('createLocationMessage', (coords) => {
-        io.emit('newLocationMessage', generateLocationMessage('Admin', coords.latitude, coords.longitude));
+        let user = users.getUser(socket.id);
+
+        if ( user ) {
+            io.to(user.room).emit('newLocationMessage', generateLocationMessage(user.name, coords.latitude, coords.longitude));
+        }
     });
 
     socket.on('disconnect', () => {
